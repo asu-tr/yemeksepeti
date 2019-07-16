476b046d2934a714df8a1ab5687a9970ec134317
Asu <29000874+asu-tr@users.noreply.github.com>
2019-07-16 23:50:19 +0300

    SANIRIM BOZDUM BAZI ŞEYLERİ
    VE İÇİM ÇOK DARALIYOR ÇOK SIKILDIM OLMUYOR DEPRESYONA GİRİYORUM GALİBA

--- Yemeksepeti/Controllers/AdminController.cs
+++ Yemeksepeti/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Yemeksepeti.App_Classes;
 using Yemeksepeti.Models;
+using Yemeksepeti.ViewModels;
 
 namespace Yemeksepeti.Controllers
 {
@@ -18,56 +19,101 @@ namespace Yemeksepeti.Controllers
         {
             return View();
         }
+        
+
 
         public ActionResult Firmalar()
         {
             return View(Context.Baglanti.Users.ToList());
         }
-        //FIRMA EKLE EKLE
+        // FIRMA EKLE EKLE
 
+        // FIRMA DUZENLE SAYFASI EKLE
 
+        // FIRMA SIL SAYFASI EKLE
+
+        
+        
         public ActionResult Urunler()
         {
-            return View();
-        }
+            var Urun = new List<Products>();
+            var Kategori = new List<Categories>();
+            var Firma = new List<Users>();
 
+            var UrunViewModel = new ProductViewModel
+            {
+                Urunler = Urun,
+                Kategoriler = Kategori,
+                Firmalar = Firma
+            };
+
+            return View(UrunViewModel);
+        }
+        // URUN EKLE EKLE
+
+        // URUN DUZENLE SAYFASI EKLE
+
+        // URUN SIL SAYFASI EKLE
+
+        
+        
         public ActionResult Menuler()
         {
-            return View();
+            return View(Context.Baglanti.Menus.ToList());
         }
+        // MENU EKLE EKLE
 
+        // MENU DUZENLE SAYFASI EKLE
+
+        // MENU SIL SAYFASI EKLE
+
+        
+        
         public ActionResult Siparisler()
         {
-            return View();
+            return View(Context.Baglanti.Orders.ToList());
         }
+        // SIPARIS DUZENLE SAYFASI EKLE
 
+        
+        
         public ActionResult Uyeler()
         {
             return View(Context.Baglanti.Users.ToList());
         }
-
         // UYE EKLE SAYFASI EKLE
 
         // UYE DUZENLE SAYFASI EKLE
 
         // UYE SIL SAYFASI EKLE
 
+        
+        
         public ActionResult Yorumlar()
         {
-            return View();
+            return View(Context.Baglanti.Comments.ToList());
         }
+        // yorum DUZENLE SAYFASI EKLE
 
+        // yorum SIL SAYFASI EKLE
+
+        
+        
         public ActionResult Konumlar()
         {
-            return View();
+            return View(Context.Baglanti.Locations.ToList());
         }
+        // KONUM EKLE SAYFASI EKLE
+
+        // KONUM DUZENLE SAYFASI EKLE
+
+        // KONUM SIL SAYFASI EKLE
+
 
         public ActionResult CikisYap()
         {
             return View();
         }
-
-
 
     }
 }
---
+++ Yemeksepeti/ViewModels/ProductViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Yemeksepeti.Models;
+
+namespace Yemeksepeti.ViewModels
+{
+    public class ProductViewModel
+    {
+        public List<Products> Urunler { get; set; }
+        public List<Categories> Kategoriler { get; set; }
+        public List<Users> Firmalar { get; set; }
+    }
+}
